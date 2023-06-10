e6ea5e2565bb8deeac19ab3f54e87ac7194eedbe
Omar Zapata <Omar.Zapata@cern.ch>
2023-06-10 02:17:29 -0500

    trying a newest numpy version for compatibility with python 3.8

--- setup.py
+++ setup.py
@@ -81,8 +81,8 @@ def main():
         # Dependent packages (distributions)
         install_requires=[
             'pymongo>=3.10.1',
-            'numpy==1.16.5; python_version=="3.8"',
-            'numpy>=1.16.5; python_version>"3.8"',
+            'numpy==1.21.0; python_version=="3.8"',
+            'numpy>=1.21.0; python_version>"3.8"',
             'pandas==2.0.2; python_version=="3.8"',
             'pandas>=2.0.2; python_version>"3.8"',
             'requests>=2.22.0',
